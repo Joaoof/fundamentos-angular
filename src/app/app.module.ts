86f56748935a35a505d0a2cd95fb4282e8fca10f
Joaoof <joaodeus400@gmail.com>
2024-06-10 09:43:57 -0300

    feat: pipe costumizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimeiroComponenteComponent } from './primeiro-componente/primeiro-componente.compenent';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+import { MultiplicarPorPipe } from './multiplicar-por.pipe';
+import { PipeCustomizadoComponent } from './pipe-customizado/pipe-customizado.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrimeiroComponenteComponent,
+    SegundoComponenteComponent,
+    MultiplicarPorPipe,
+    PipeCustomizadoComponent
   ],
   imports: [
     BrowserModule,
